756e4dead85b7f4708fac330172443459bfded24
kevincbase <kevin@cbase.net>
2018-02-20 16:24:36 +0000

    Add project files.

---
+++ TestingNinject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TestingNinject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
